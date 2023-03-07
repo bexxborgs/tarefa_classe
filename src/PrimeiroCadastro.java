7a6932a39159071b15236f82a6e10b6a8cbff28d
Rebeca Borges <87546461+bexxborgs@users.noreply.github.com>
2023-03-07 10:10:47 -0300

    Update PrimeiroCadastro.java

--- src/PrimeiroCadastro.java
+++ src/PrimeiroCadastro.java
@@ -1,9 +1,9 @@
 public class PrimeiroCadastro {
 
-    public static void main(String args []){
-        System.out.println("Olá Aluno");
-        Alunos aluno = new Alunos();
-        aluno.cadastrarEndereco();
+   // public static void main(String args []){
+   //     System.out.println("Olá Aluno");
+   //   Alunos aluno = new Alunos();
+   //   aluno.cadastrarEndereco();
 
     }
 }
